b950e07149eae4ad5ed87fb74af0bfa0338180b3
chad-daniel-masella <chadm2886@gmail.com>
2020-10-13 21:36:24 +0200

    .Net Framework learning from basics

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// a "namespace" is 
+namespace HelloWorld
+{
+    class Program
+    {
+        // this block of code is a method called "Main"
+        // methods needs to reside inside a class that is inside a namespace
+        static void Main(string[] args)
+        {
+            // this is a comment
+
+            // this is a WriteLine function that takes in parameter to output
+            // the semi-colon ends a statement
+            // "WriteLine" is a member of the "Console" class
+            Console.WriteLine("Hello World");
+
+            // this is a ReadLine function that takes in input
+            // the "Console" class was written by someone in microsoft (part of the Class Library)
+            // the "Console" class has a method called "ReadLine"
+            Console.ReadLine();
+
+            // === === the .Net Framework === ===
+
+            // === Class Library: ===
+            // Library code that microsoft wrote so that we as developers dont have to worry about them
+            // library code to help with math operations, working with text, etc
+
+            // when we used the "Console.WriteLine("Hello World");"
+            // we dont care how to write the text all we care about is that the text is written to the console
+            // we used a class library "Console"
+
+            // === The Runtime / The Common Language Runtime (CLR): ===
+            // takes care of low level details, so that the developer can focus on what the application does
+            // and less with how to interact with a computers OS, memory, hardware, etc
+
+            // what is a .Net Assembly
+        }
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+              this is a multi line comment
+
+             */
+
+            /*
+            // declared variables
+            // variable types: integer (-2 billion 147 to 2 billion 147)
+            int x;
+            int y;
+
+            // assign / manipulate varaibles
+            // "=" is an assignment operator 
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            */
+
+            /*
+            // variable declaration using camel-case
+            // C# is case sensative
+            string myFirstName;
+
+            // you need to initialize a var variable with a value, even if it is an empty string
+            var myfirstname = "Tom";
+
+            myFirstName = "Bob";
+
+            Console.WriteLine(myFirstName, myfirstname);
+
+            Console.ReadLine();
+            */
+
+            // explicit convertion using string and integer
+            // explicit conversion means we will have to write the code in order to perform the data conversion
+            int x = 7;
+            // string y = "Bob";
+            string y = "5";
+            string myFirstTry = x.ToString() + y;
+
+            // int mySecondTry = x + y;
+            int mySecondTry = x + int.Parse(y);
+
+
+            Console.WriteLine(myFirstTry);
+        }
+    }
+}
