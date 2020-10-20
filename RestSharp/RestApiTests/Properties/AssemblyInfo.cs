5bd3ee6fd0cc460caca3d034334cd5e39a0efee2
chad-daniel-masella <chadm2886@gmail.com>
2020-10-20 23:06:09 +0200

    Added RestSharp Test Cases

---
+++ RestSharp/RestApiTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestApiTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestApiTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1aef89b7-e0ad-4b88-8ba9-d4001fd04652")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RestSharp/RestApiTests/StatusCodeTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace RestApiTests
+{
+    [TestClass]
+    public class StatusCodeTests
+    {
+        [TestMethod]
+        public void GetAllZipCodes()
+        {
+            // arrange the API Request data
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest("us/90210", Method.GET);
+
+            // execute the API using the arranged API request data
+            IRestResponse response = client.Execute(request);
+
+            // assert
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                Assert.Fail("Test Case Failure");
+            }
+            
+        }
+
+        [TestMethod]
+        public void GetZipCode()
+        {
+            // arrange the API Request data
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest("us/90210", Method.GET);
+
+            // execute the API using the arranged API request data
+            IRestResponse response = client.Execute(request);
+
+            // assert
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                Assert.Fail("Test Case Failure");
+            }
+
+            Trace.WriteLine(response.Content);
+
+        }
+    }
+}
