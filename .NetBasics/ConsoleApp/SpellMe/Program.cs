71e0d5dfcdf26504254ebe71306b186881c3c244
Chad <chadm2886@gmail.com>
2021-01-11 22:05:47 +0200

    Added initial file configurations

---
+++ .NetBasics/ConsoleApp/SpellMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpellMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a value an I will spell it out on the console");
+        }
+    }
+}
